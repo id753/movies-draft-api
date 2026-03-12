7c2bbca0613a2760322e183ad058c46b57c5f40d
Eugene O. (id753) <mimywhy@gmail.com>
2026-03-12 09:06:53 +0200

    middleware holder

---
+++ src/models/draft.js
@@ -0,0 +1,38 @@
+import { Schema, model } from 'mongoose';
+
+const draftSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'Title is required'],
+      trim: true,
+      maxlength: [100, 'Title cannot be more than 100 characters'],
+    },
+    content: {
+      type: String,
+      required: [true, 'Content is required'],
+    },
+    categoryId: {
+      type: String,
+      required: [true, 'Category ID is required'],
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User', // Ссылка на модель User
+      required: true,
+    },
+    tags: {
+      type: String, // В твоем типе Draft это строка
+      default: '',
+    },
+  },
+  {
+    timestamps: true, // Автоматически создаст createdAt и updatedAt
+    versionKey: false, // Убирает поле __v
+  },
+);
+
+// Создаем модель (с проверкой, чтобы не пересоздавать ее при Hot Reload в Next.js)
+export const Draft = model('Draft', draftSchema);
+
+export default Draft;
--- src/server.js
+++ src/server.js
@@ -1,53 +1,42 @@
 import express from 'express';
 import cors from 'cors';
-import pino from 'pino-http';
 import 'dotenv/config';
 import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+
+import { Draft } from './models/draft.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-// Middleware
-app.use(express.json());
+// Глобальні middleware
+app.use(logger); // 1. Логер першим — бачить усі запити // Логування запитів
+app.use(express.json()); // Middleware для парсингу JSON
 app.use(cors()); // Дозволяє запити з будь-яких джерел
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat:
-          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello world!' });
+// GET ВСЕХ
+app.get('/drafts', async (req, res) => {
+  const drafts = await Draft.find();
+  res.status(200).json(drafts);
+});
+// GET ОДНОГО
+app.get('/drafts/:id', async (req, res) => {
+  const { id } = req.params;
+  const draft = await Draft.findById(id);
+  if (!draft) {
+    return res.status(404).json({ message: 'draft not found' });
+  }
+  res.status(200).json(draft);
 });
 
 // Middleware 404
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
+app.use(notFoundHandler);
 
 // Middleware для обробки помилок
-app.use((err, req, res, next) => {
-  console.error(err);
+app.use(errorHandler);
 
-  const isProd = process.env.NODE_ENV === 'production';
-
-  res.status(500).json({
-    message: isProd
-      ? 'Something went wrong. Please try again later.'
-      : err.message,
-  });
-});
 await connectMongoDB();
 
 // Запуск сервера
